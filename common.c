17365e5068d9405f9c16f39bebda99bb9424df7e
Andrej Shadura <andrew@shadura.me>
2011-03-01 21:59:06 +0200

    reindent some more sources

--- common.c
+++ common.c
@@ -18,39 +18,46 @@
 #include <netinet/in.h>
 
 /* Globals */
-int loglevel = MSGERR;    /* The default logging level is to only log
-                             error messages */
-char logfilename[256];    /* Name of file to which log messages should
-                             be redirected */
-FILE *logfile = NULL;     /* File to which messages should be logged */
-int logstamp = 0;         /* Timestamp (and pid stamp) messages */
+int loglevel = MSGERR;          /* The default logging level is to only log
+                                   error messages */
+char logfilename[256];          /* Name of file to which log messages should
+                                   be redirected */
+FILE *logfile = NULL;           /* File to which messages should be logged */
+int logstamp = 0;               /* Timestamp (and pid stamp) messages */
 
-unsigned int resolve_ip(char *host, int showmsg, int allownames) {
-	struct hostent *new;
-	unsigned int	hostaddr;
-	struct in_addr *ip;
+unsigned int resolve_ip(char *host, int showmsg, int allownames)
+{
+    struct hostent *new;
+    unsigned int hostaddr;
+    struct in_addr *ip;
 
-	if ((hostaddr = inet_addr(host)) == (unsigned int) -1) {
-		/* We couldn't convert it as a numerical ip so */
-		/* try it as a dns name                        */
-		if (allownames) {
-			#ifdef HAVE_GETHOSTBYNAME
-			if ((new = gethostbyname(host)) == (struct hostent *) 0) {
-			#endif
-				return(-1);
-			#ifdef HAVE_GETHOSTBYNAME
-			} else {
-				ip = ((struct in_addr *) * new->h_addr_list);
-				hostaddr = ip -> s_addr;
-				if (showmsg) 
-					printf("Connecting to %s...\n", inet_ntoa(*ip));
-			}
-			#endif
-		} else
-			return(-1);
-	}
+    if ((hostaddr = inet_addr(host)) == (unsigned int)-1)
+    {
+        /* We couldn't convert it as a numerical ip so */
+        /* try it as a dns name                        */
+        if (allownames)
+        {
+#ifdef HAVE_GETHOSTBYNAME
+            if ((new = gethostbyname(host)) == (struct hostent *)0)
+            {
+#endif
+                return (-1);
+#ifdef HAVE_GETHOSTBYNAME
+            }
+            else
+            {
+                ip = ((struct in_addr *)*new->h_addr_list);
+                hostaddr = ip->s_addr;
+                if (showmsg)
+                    printf("Connecting to %s...\n", inet_ntoa(*ip));
+            }
+#endif
+        }
+        else
+            return (-1);
+    }
 
-	return (hostaddr);
+    return (hostaddr);
 }
 
 /* Set logging options, the options are as follows:             */
@@ -64,68 +71,74 @@ unsigned int resolve_ip(char *host, int showmsg, int allownames) {
 /*             be logged instead of to standard error           */
 /*  timestamp - This indicates that messages should be prefixed */
 /*              with timestamps (and the process id)            */
-void set_log_options(int level, char *filename, int timestamp) {
+void set_log_options(int level, char *filename, int timestamp)
+{
 
-   loglevel = level;
-   if (loglevel < MSGERR)
-      loglevel = MSGNONE;
+    loglevel = level;
+    if (loglevel < MSGERR)
+        loglevel = MSGNONE;
 
-   if (filename) {
-      strncpy(logfilename, filename, sizeof(logfilename));
-      logfilename[sizeof(logfilename) - 1] = '\0';
-   }
+    if (filename)
+    {
+        strncpy(logfilename, filename, sizeof(logfilename));
+        logfilename[sizeof(logfilename) - 1] = '\0';
+    }
 
-   logstamp = timestamp;
+    logstamp = timestamp;
 }
 
-void show_msg(int level, char *fmt, ...) {
-	va_list ap;
-	int saveerr;
-	extern char *progname;
-   char timestring[20];
-   time_t timestamp;
+void show_msg(int level, char *fmt, ...)
+{
+    va_list ap;
+    int saveerr;
+    extern char *progname;
+    char timestring[20];
+    time_t timestamp;
 
-   if ((loglevel == MSGNONE) || (level > loglevel))
-      return;
+    if ((loglevel == MSGNONE) || (level > loglevel))
+        return;
 
-   if (!logfile) {
-      if (logfilename[0]) {
-         logfile = fopen(logfilename, "a");
-         if (logfile == NULL) {
+    if (!logfile)
+    {
+        if (logfilename[0])
+        {
+            logfile = fopen(logfilename, "a");
+            if (logfile == NULL)
+            {
+                logfile = stderr;
+                show_msg(MSGERR, "Could not open log file, %s, %s\n", logfilename, strerror(errno));
+            }
+        }
+        else
             logfile = stderr;
-            show_msg(MSGERR, "Could not open log file, %s, %s\n", 
-                     logfilename, strerror(errno));
-         }
-      } else
-         logfile = stderr;
-   }
+    }
 
-   if (logstamp) {
-      timestamp = time(NULL);
-      strftime(timestring, sizeof(timestring),  "%H:%M:%S", 
-               localtime(&timestamp));
-      fprintf(logfile, "%s ", timestring);
-   }
+    if (logstamp)
+    {
+        timestamp = time(NULL);
+        strftime(timestring, sizeof(timestring), "%H:%M:%S", localtime(&timestamp));
+        fprintf(logfile, "%s ", timestring);
+    }
 
-   fputs(progname, logfile);
+    fputs(progname, logfile);
 
-   if (logstamp) {
-      fprintf(logfile, "(%d)", getpid());
-   }
-   
-   fputs(": ", logfile);
-	
-	va_start(ap, fmt);
+    if (logstamp)
+    {
+        fprintf(logfile, "(%d)", getpid());
+    }
 
-	/* Save errno */
-	saveerr = errno;
+    fputs(": ", logfile);
 
-	vfprintf(logfile, fmt, ap);
-	
-   fflush(logfile);
+    va_start(ap, fmt);
 
-	errno = saveerr;
+    /* Save errno */
+    saveerr = errno;
 
-	va_end(ap);
+    vfprintf(logfile, fmt, ap);
+
+    fflush(logfile);
+
+    errno = saveerr;
+
+    va_end(ap);
 }
-
--- tsocks.h
+++ tsocks.h
@@ -7,54 +7,57 @@
 #include <parser.h>
 
 /* Structure representing a socks connection request */
-struct sockreq {
-   int8_t version;
-   int8_t command;
-   int16_t dstport;
-   int32_t dstip;
-   /* A null terminated username goes here */
+struct sockreq
+{
+    int8_t version;
+    int8_t command;
+    int16_t dstport;
+    int32_t dstip;
+    /* A null terminated username goes here */
 };
 
 /* Structure representing a socks connection request response */
-struct sockrep {
-   int8_t version;
-   int8_t result;
-   int16_t ignore1;
-   int32_t ignore2;
+struct sockrep
+{
+    int8_t version;
+    int8_t result;
+    int16_t ignore1;
+    int32_t ignore2;
 };
 
 /* Structure representing a socket which we are currently proxying */
-struct connreq {
-   /* Information about the socket and target */
-   int sockid;
-   struct sockaddr_in connaddr;
-   struct sockaddr_in serveraddr;
+struct connreq
+{
+    /* Information about the socket and target */
+    int sockid;
+    struct sockaddr_in connaddr;
+    struct sockaddr_in serveraddr;
 
-   /* Pointer to the config entry for the socks server */
-   struct serverent *path;
+    /* Pointer to the config entry for the socks server */
+    struct serverent *path;
 
-   /* Current state of this proxied socket */
-   int state;
+    /* Current state of this proxied socket */
+    int state;
 
-   /* Next state to go to when the send or receive is finished */
-   int nextstate;
+    /* Next state to go to when the send or receive is finished */
+    int nextstate;
 
-   /* When connections fail but an error number cannot be reported 
-    * because the socket is non blocking we keep the connreq struct until
-    * the status is queried with connect() again, we then return
-    * this value */
-   int err;
+    /* When connections fail but an error number cannot be reported 
+     * because the socket is non blocking we keep the connreq struct until
+     * the status is queried with connect() again, we then return
+     * this value */
+    int err;
 
-   /* Events that were set for this socket upon call to select() or
-    * poll() */
-   int selectevents;
+    /* Events that were set for this socket upon call to select() or
+     * poll() */
+    int selectevents;
 
-   /* Buffer for sending and receiving on the socket */
-   int datalen;
-   int datadone;
-   char buffer[1024];
+    /* Buffer for sending and receiving on the socket */
+    int datalen;
+    int datadone;
+    char buffer[1024];
 
-   struct connreq *next;
+    struct connreq *next;
 };
 
 /* Connection statuses */
@@ -64,16 +67,16 @@ struct connreq {
 #define SENDING 3
 #define RECEIVING 4
 #define SENTV4REQ 5
-#define GOTV4REQ 6 
-#define SENTV5METHOD 7 
+#define GOTV4REQ 6
+#define SENTV5METHOD 7
 #define GOTV5METHOD 8
 #define SENTV5AUTH 9
 #define GOTV5AUTH 10
 #define SENTV5CONNECT 11
 #define GOTV5CONNECT 12
-#define DONE 13 
-#define FAILED 14 
-   
+#define DONE 13
+#define FAILED 14
+
 /* Flags to indicate what events a socket was select()ed for */
 #define READ (1<<0)
 #define WRITE (1<<1)
