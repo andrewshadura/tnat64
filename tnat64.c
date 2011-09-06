ae6f04801c375d2dc41840790401e1371c6be5b0
Andrej Shadura <andrew@shadura.me>
2011-09-06 20:12:41 +0300

    change the spelling of my last name

--- tnat64.c
+++ tnat64.c
@@ -1,7 +1,7 @@
 /*
 
     TNAT64 - Wrapper library for redirecting IPv4 connections to NAT64
-    Copyright (C) 2011 Andrew O. Shadoura
+    Copyright (C) 2011 Andrew O. Shadura
     Based on original code of TSOCKS, copyright (C) 2000 Shaun Clowes
 
     This program is free software; you can redistribute it and/or modify
